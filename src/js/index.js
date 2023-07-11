8871924ef9c2f3492744c1c4dcd9562cf1905a47
Rafaelly <138537427+devrafaelly@users.noreply.github.com>
2023-07-11 02:14:51 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach(personagem => {
+    personagem.addEventListener('mouseenter', () => {
+
+        funcionarNoCelular();
+
+        removerSelecaoDoPersonagem();
+
+        personagem.classList.add('selecionado');
+
+        alterarImagemPersonagemSelecionado(personagem);
+
+        alterarNomeDoPersonagemSelecionado(personagem);
+
+        alterarDescricaoDoPersonagemSelecionado(personagem);
+    })
+});
+
+
+function funcionarNoCelular() {
+    if (window.innerWidth < 450) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+}
+
+function alterarDescricaoDoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomeDoPersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
